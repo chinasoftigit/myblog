cc0a9218632763779e755b876a9a5b00fa56f855
chinasoftigit <1870633559@qq.com>
2018-06-22 20:34:10 +0800

    222222

--- mynews/src/com/chinasofti/test/Test2.java
+++ mynews/src/com/chinasofti/test/Test2.java
@@ -3,8 +3,9 @@ package com.chinasofti.test;
 public class Test2 {
 
 	public static void main(String[] args) {
+		System.out.println("111111111111");
 		System.out.println("222222222222");
-
+		System.out.println("333333333333");
 	}
 
 }
