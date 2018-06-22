32e6ea93211afb3222eff7a78fede0d0f63c08c6
chinasoftigit <1870633559@qq.com>
2018-06-22 20:59:43 +0800

    3333333

--- myweb/src/com/chinasofti/test/Test3.java
+++ myweb/src/com/chinasofti/test/Test3.java
@@ -4,7 +4,8 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		System.out.println("333333333333");
-
+		System.out.println("444444444444");
+		System.out.println("555555555555");
 	}
 
 }
